dbad82e537c1f1245265b4422409cc04f4e2338c
Joey <imjoeydhillon@gmail.com>
2024-10-06 18:43:25 -0500

    fourth commit

--- lab18.cpp
+++ lab18.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
-struct Node
+struct Movie
 {
     float rating;
     string comments;
-    Node *next;
+    Movie *next;
 
 };
 
 
 int main(int argc, char const *argv[]){
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+
     return 0;
+}
+
+Movie* buildMovie(){
+    char AnotherReview = 'y';
+
+    Movie* head = nullptr;
+
+    // Check's if the user want's to continue
+    while(tolower(AnotherReview) != 'n'){
+        string tempInt;
+
+        cout << "Enter review rating 0-5: ";
+        cin >> tempInt;
+
+    }
+
+    
+
+}
+
+void addHead(Movie* head){
+    while(head){
+        head = head->next;
+    }
 }
 
 //Returns Movie at given index
