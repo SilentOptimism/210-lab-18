092bc1362473751cff6878bd84a03b8e3e6c0bff
Joey <imjoeydhillon@gmail.com>
2024-10-06 18:21:32 -0500

    second commit

--- lab18.cpp
+++ lab18.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+struct Node
+{
+    float rating;
+    string comments;
+    Node *next;
+
+};
+
+
 int main(int argc, char const *argv[]){
     cout << "test";
     return 0;
